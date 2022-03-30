f3f472a958d332dc811b80fa7819ebbf69c01be6
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-30 23:22:56 +0000

    create class MusicAlbum

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,1 +1,18 @@
-# music album
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify, :archived, :name
+  attr_reader :publish_date
+
+  def initialize(name, publish_date, on_spotify)
+    super(id: nil, publish_date: publish_date)
+    @name = name
+    @on_spotify = on_spotify
+  end
+
+  private
+
+  def can_be_archived?
+    archived && @on_spotify
+  end
+end
