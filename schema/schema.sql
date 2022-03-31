e735bda707101677f54a7d8dcc5313c8f65c307d
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-31 10:35:59 +0000

    music_albums table

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE catalog_of_my_things;
+
 CREATE TABLE genres (
 	id  INT GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(100),
@@ -15,4 +17,11 @@ CREATE TABLE item (
 	FOREIGN KEY (genre_id) REFERENCES genres (id),
 	FOREIGN KEY (author_id) REFERENCES author (id),
 	FOREIGN KEY (label_id) REFERENCES labels (id)
+);
+
+CREATE TABLE music_albums (
+	id  INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100),
+	on_spotify BOOLEAN,
+	FOREIGN KEY(id) REFERENCES item(id)
 );
