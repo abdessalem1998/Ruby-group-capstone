f83072ee594fb2244f3b501168d8f21f06045cf3
kingqabil <kingqabil@live.com>
2022-03-31 18:36:29 +0300

    Create Game and Author classes

--- classes/game.rb
+++ classes/game.rb
@@ -1,1 +1,16 @@
-# game
+require './item'
+require 'date'
+
+class Game
+
+    def initialize(multiplayer, last_played_at)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+
+    def can_be_archived?
+        now = Date.now
+        timeElapsed = (now - last_played_at).to_i
+        super() and (timeElapsed > 2.year)
+    end
+end
