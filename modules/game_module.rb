beecc6fb7b174e6c12175f7aa647ae09646e6abc
kingqabil <kingqabil@live.com>
2022-03-31 22:52:30 +0300

    Update Author JSON file

--- modules/game_module.rb
+++ modules/game_module.rb
@@ -9,15 +9,28 @@ module GameModule
     puts "\nENTER GAME DETAILS\n\n"
 
     game_name = capture_input('Input game name')
-    publish_date = capture_input('Enter publish date')
     last_played_at = capture_input('Input Last played date')
     multiplayer = capture_input('Is the game Multiplayer? (enter Y for "Yes" evertyhing else will be treated as "No"')
 
     multiplayer = multiplayer == 'Y' || true
 
-    @app.add_game([game_name, last_played_at, multiplayer, publish_date])
+    @app.add_game([game_name, last_played_at, multiplayer])
 
     puts "\nNew Game Added!"
+    puts "\n-------------------------------"
+
+    add_new_author_details
+  end
+
+  def add_new_author_details
+    puts "\n-------------------------------"
+    puts "\nENTER AUTHOR DETAILS\n\n"
+
+    first_name = capture_input('Input Author Name')
+    last_name = capture_input('Input Author Last Name')
+
+    @app.add_author([first_name, last_name])
+    puts "\nNew Author Added!"
     puts "\n-------------------------------"
   end
 end
