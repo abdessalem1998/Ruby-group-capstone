a1abcd19c5838d36241bbc1fda8283f073a11326
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-03-31 11:37:59 +0000

    add tests for genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,1 +1,22 @@
-# tests
+require_relative '../classes/genre'
+require_relative '../classes/item'
+
+describe 'Genre test' do
+  context 'Should create a new genre' do
+    new_item = Item.new(id: nil, publish_date: Date.parse('2000/11/20'))
+    genre = Genre.new('Rock and Roll')
+
+    it 'creates a new genre' do
+      expect(genre.name).to eq 'Rock and Roll'
+    end
+
+    it 'add a new item' do
+      genre.add_item(new_item)
+      expect(genre.items.length).to eq 1
+    end
+
+    it 'has a publish date' do
+      expect(new_item.publish_date).to eq Date.parse('2000/11/20')
+    end
+  end
+end
